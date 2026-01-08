3dd83bd36477c5a8b3155000f5b0a4000b748f98
James <James@Blonden.co.uk>
2026-01-08 19:35:13 +0000

    Initial Commit

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by James Blonden on 08/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
